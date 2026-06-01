c8b97c8869493673886e543b686c00809f8facfd
Rui <rui.resende.pinto@gmail.com>
2026-06-01 18:48:42 +0100

    changes to automatic workflow

--- automatic_workflow.R
+++ automatic_workflow.R
@@ -13,14 +13,15 @@ library(R.utils)
 source('utils.R')
 source('config.R')
 
-args = commandArgs(trailingOnly=FALSE, asValues=TRUE)
-print(args)
+#args = commandArgs(trailingOnly=FALSE, asValues=TRUE)
+#print(args)
 
+for (file in files2process){
 df <- read.csv(
-  args$file1, 
+  file, 
   header = input_header,
   sep = input_sep,
-  quote = input_quote
+  #quote = input_quote
 )
 
 # every occurrence must have corresponding coordinates
@@ -40,7 +41,7 @@ if (input_grid_source == "custom") {
 grid_crs <- st_crs(4326)
 
 # join user-defined columns for aggregating with the necessary eeacellcode that will be defined from the coordinates
-aggregate_cols <- c("eeacellcode", input$aggregate_cols)
+aggregate_cols <- c("eeacellcode", input_aggregate_cols)
 
 if (isFALSE(input_use_custom_uncertainty)){
   
@@ -74,4 +75,8 @@ floppydatacube <- floppydisk2cube(data_in = corrected_uncertainty,
                                   seed=input_seed)
 
 #outputfilename should be args
-write.csv(floppydatacube, args$output, row.names = FALSE, quote = F )
+output_name <- gsub('temp', 'cube', file)
+write.csv(floppydatacube, output_name, row.names = FALSE, quote = F )
+}
+
+files2process <- list.files('../easyTransfer-WO0md7Hl(3)/', pattern='.temp.csv', full.names=TRUE)
